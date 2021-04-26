465ac9b9abc060b5ecde466854d1c2f210847673
moawedx <mohamed.moawedx1@gmail.com>
2021-04-26 21:45:13 +0200

    PROJECT

---
+++ AMIT PROJECT/Master_state/Master_state/ADC.h
@@ -0,0 +1,19 @@
+/*
+ * ADC.h
+ *
+ * Created: 2/5/2021 9:51:38 PM
+ *  Author: karim
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+
+/*Include configuration file for ADC*/
+#include "ADC_CONFIG.h"
+/*Function prototypes*/
+void ADC_Init(Uint8t channel);/*Initialize ADC*/
+Uint16t ADC_Read(void);/*Read value of current ADC*/
+
+
+#endif /* ADC_H_ */
